35cc19b3a75b429699b90701cc89238917b5b0e8
newuser <firstGodKing@163.com>
2019-11-12 10:51:14 +0800

    创建动物类

---
+++ index.js
@@ -0,0 +1,16 @@
+class Animal {
+
+    constructor (name, color){
+        this.name = name;
+        this.color = color;
+    }
+
+    running(){
+        console.log(this.name + '会跑步');
+    }
+
+};
+
+let tiger = new Animal('老虎', '金色');
+
+tiger.running();
